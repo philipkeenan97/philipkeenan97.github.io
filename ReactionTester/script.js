9a90c8997e3a48994fb68c267614059bd15e7d94
philipkeenan97 <pkeenan97@gmail.com>
2021-08-19 16:03:47 +0100

    Major IMprovements to FYP commit

---
+++ ReactionTester/script.js
@@ -0,0 +1,51 @@
+function getRandomColor() {
+      var letters = '0123456789ABCDEF'.split('');
+      var color = '#';
+       for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+       }
+      return color;
+}
+
+setInterval((function(){
+   var start = Date.now();
+   var textNode = document.createTextNode('0');
+   document.getElementById('seconds').appendChild(textNode);
+   return function() {
+        textNode.data = Math.floor((Date.now()-start)/1000);
+        };
+   }()), 1000);
+
+function makeShapeAppear() {
+       var top = Math.random() * 600;
+       var left = Math.random() * 1100;
+      var width = (Math.random() * 200) + 100;
+      if (Math.random() > 0.5) {
+            document.getElementById("shape").style.borderRadius = "50%";
+      } else {
+            document.getElementById("shape").style.borderRadius = "0";
+      }
+       document.getElementById("shape").style.backgroundColor = getRandomColor();
+       document.getElementById("shape").style.width = width + "px";
+      document.getElementById("shape").style.height = width + "px";
+       document.getElementById("shape").style.top = top + "px";
+      document.getElementById("shape").style.left = left + "px";
+      document.getElementById("shape").style.display = "block";
+      start = new Date().getTime();
+}
+
+function appearAfterDelay() {
+      setTimeout(makeShapeAppear, Math.random() * 2000);
+}
+
+
+var start = new Date().getTime();
+appearAfterDelay();
+
+document.getElementById("shape").onclick = function() {
+      document.getElementById("shape").style.display = "none";
+      var end = new Date().getTime();
+      var timeTaken = (end - start) / 1000;
+      document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+      appearAfterDelay();
+}
